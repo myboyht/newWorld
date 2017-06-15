6c41e671145df4d9ba8b825f8201bc991604b058
myboyht <727030994@qq.com>
2017-06-15 11:44:59 +0800

    小提示弹出层
    引入JS css 
    var tips = new opentips();
    tips.tipopen('hello',2000) //输入的内容，显示时间 不输入时间默认为2S

---
+++ htpoentips/JS/httips.js
@@ -0,0 +1,29 @@
+/*depend by jquery*/
+
+
+function opentips(tipstell,timeout){
+		if(tipstell==undefined){tipstell=''};
+		this.tipopen=function(tipstell,timeout){
+			if(timeout==undefined){timeout=2000};
+			this.timeout = timeout;
+			this.tipsfont=tipstell;
+			this.temp = '<div class="ht-tips-errorbox">'+
+						'<div class="tips top-in">'+
+							this.tipsfont+
+						'</div>'+
+						'</div>'	
+			
+			if($('body').has('.errorbox').get(0)){
+				return
+			}else{
+			$('body').append(this.temp)
+			var t1 = setTimeout(function(){
+				$(".ht-tips-errorbox").addClass('optyhide');
+				var t2 =setTimeout(function(){
+					$('body .errorbox').remove();
+				},400)
+			},this.timeout)
+			}
+		}
+						
+	}
